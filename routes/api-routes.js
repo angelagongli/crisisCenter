02def731d0203296c800046d066167cd335a6473
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-04-12 19:29:33 -0500

    Fixed merge errors on merging with all of the bookmars and emails stuff.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,7 +5,6 @@ var exphbs = require("express-handlebars");
 var axios = require("axios");
 var Twitter = require("twitter");
 
-
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
@@ -45,17 +44,25 @@ module.exports = function(app) {
       recipientID: req.body.recipientID
     })
       .then(function(data) {
-        const sgMail = require('@sendgrid/mail');
+        const sgMail = require("@sendgrid/mail");
         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-        let textString = req.body.message + " -- Sent by " + 
-          req.body.userName + " (" + 
-          req.body.userEmail + "): " + 
+        let textString =
+          req.body.message +
+          " -- Sent by " +
+          req.body.userName +
+          " (" +
+          req.body.userEmail +
+          "): " +
           req.body.text;
 
-        let HTMLString = req.body.message + "<br><br>Sent by " + 
-          req.body.userName + " (" + 
-          req.body.userEmail + ")<br><br>" + 
+        let HTMLString =
+          req.body.message +
+          "<br><br>Sent by " +
+          req.body.userName +
+          " (" +
+          req.body.userEmail +
+          ")<br><br>" +
           req.body.content;
 
         const msg = {
@@ -63,18 +70,19 @@ module.exports = function(app) {
           from: "coronacrisiscenter@gmail.com",
           subject: req.body.subject,
           text: textString,
-          html: HTMLString,
+          html: HTMLString
         };
-        
-        sgMail
-          .send(msg)
-          .then(() => {}, error => {
+
+        sgMail.send(msg).then(
+          () => {},
+          error => {
             console.error(error);
-         
+
             if (error.response) {
-              console.error(error.response.body)
+              console.error(error.response.body);
             }
-          });
+          }
+        );
 
         res.json(data);
       })
@@ -97,7 +105,7 @@ module.exports = function(app) {
         res.status(401).json(err);
       });
   });
-  
+
   // Route for logging user out
   app.get("/logout", function(req, res) {
     req.logout();
@@ -144,94 +152,105 @@ module.exports = function(app) {
   });
 };
 
-  app.get("/api/user_family_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      db.User.findAll({
-        attributes : ["name", "email", "id"],
-        where : { FamilyId: req.user.FamilyId }
-      }).then(data => {
+app.get("/api/user_family_data", function(req, res) {
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.json({});
+  } else {
+    db.User.findAll({
+      attributes: ["name", "email", "id"],
+      where: { FamilyId: req.user.FamilyId }
+    })
+      .then(data => {
         res.json(data);
-      }).catch(function(err) {
-        res.status(401).json(err);
-      });
-    }
-  });
-
-  app.get("/api/emails/outbox", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      db.Email.findAll({
-        where : {UserId: req.user.id }
-      }).then(data => {
-        res.json(data);
-      }).catch(function(err) {
-        res.status(401).json(err);
-      });
-    }
-  });
-  
-  app.get("/api/emails/inbox", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      db.Email.findAll({
-        where : {recipientID: req.user.id }
-      }).then(data => {
-        res.json(data);
-      }).catch(function(err) {
-        res.status(401).json(err);
-      });
-    }
-  });
-
-  app.get("/api/bookmarks", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      db.Bookmark.findAll({
-        where : {UserId: req.user.id }
-      }).then(data => {
-        res.json(data);
-      }).catch(function(err) {
-        res.status(401).json(err);
-      });
-    }
-  });
-
-  app.get("/api/nytimes", function(req, res) {
-    let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=7zg49YbZEaOjLSJIGYvO0nYJbHwmIQHa&q=coronavirus"
-    
-    axios.get(queryURL)
-      .then(response => {
-        console.log(response.data);
-        res.json(response.data);
       })
       .catch(function(err) {
         res.status(401).json(err);
       });
+  }
+});
+
+app.get("/api/emails/outbox", function(req, res) {
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.json({});
+  } else {
+    db.Email.findAll({
+      where: { UserId: req.user.id }
+    })
+      .then(data => {
+        res.json(data);
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
+  }
+});
+
+app.get("/api/emails/inbox", function(req, res) {
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.json({});
+  } else {
+    db.Email.findAll({
+      where: { recipientID: req.user.id }
+    })
+      .then(data => {
+        res.json(data);
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
+  }
+});
+
+app.get("/api/bookmarks", function(req, res) {
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.json({});
+  } else {
+    db.Bookmark.findAll({
+      where: { UserId: req.user.id }
+    })
+      .then(data => {
+        res.json(data);
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
+  }
+});
+
+app.get("/api/nytimes", function(req, res) {
+  let queryURL =
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=7zg49YbZEaOjLSJIGYvO0nYJbHwmIQHa&q=coronavirus";
+
+  axios
+    .get(queryURL)
+    .then(response => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function(err) {
+      res.status(401).json(err);
+    });
+});
+
+app.get("/tweets", function(req, res) {
+  var client = new Twitter({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_token_key,
+    access_token_secret: process.env.access_token_secret
   });
 
-};
-
-  app.get("/tweets", function(req, res) {
-    var client = new Twitter({
-      consumer_key: process.env.consumer_key,
-      consumer_secret: process.env.consumer_secret,
-      access_token_key: process.env.access_token_key,
-      access_token_secret: process.env.access_token_secret
-    });
-
-    client.get('lists/statuses.json', {list_id: '1246463879271657474'}, function(error, tweets, response) {
-      if(error) console.log(error);
-      console.log(tweets); 
-        res.json(tweets);
-    });
-})};
-
+  client.get(
+    "lists/statuses.json",
+    { list_id: "1246463879271657474" },
+    function(error, tweets, response) {
+      if (error) console.log(error);
+      console.log(tweets);
+      res.json(tweets);
+    }
+  );
+});
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,7 +4,6 @@ var db = require("../models");
 var axios = require("axios");
 var Twitter = require("twitter");
 
-
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
@@ -24,53 +23,57 @@ module.exports = function(app) {
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
- 
-  app.get("/members", isAuthenticated, function(req, res){
+
+  app.get("/members", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
-  })
-
-    app.get("/staybusy", isAuthenticated, function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/staybusy.html"));
-    });
-
-
-    app.get("/forum", isAuthenticated, function(req, res) {
-      // If the user is logged in, send them to the forum page
-      res.sendFile(path.join(__dirname, "../public/forum.html"));
-    });
-    app.get("/post", isAuthenticated, function(req, res) {
-      // If the user is logged in, send them to the forum page
-      res.sendFile(path.join(__dirname, "../public/post.html"));
-    });
   });
 
-app.get("/bookmarks", isAuthenticated, function(req, res) {
+  app.get("/staybusy", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/staybusy.html"));
+  });
+
+  app.get("/forum", isAuthenticated, function(req, res) {
+    // If the user is logged in, send them to the forum page
+    res.sendFile(path.join(__dirname, "../public/forum.html"));
+  });
+  app.get("/post", isAuthenticated, function(req, res) {
+    // If the user is logged in, send them to the forum page
+    res.sendFile(path.join(__dirname, "../public/post.html"));
+  });
+
+  app.get("/bookmarks", isAuthenticated, function(req, res) {
     db.Bookmark.findAll({
-      where : {UserId: req.user.id }
-    }).then(data => {
-      res.render("bookmarks", { bookmarks: data });
-    }).catch(function(err) {
-      res.status(401).json(err);
-    });
+      where: { UserId: req.user.id }
+    })
+      .then(data => {
+        res.render("bookmarks", { bookmarks: data });
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
   });
 
   app.get("/inbox", isAuthenticated, function(req, res) {
     db.Email.findAll({
-      where : {recipientID: req.user.id }
-    }).then(data => {
-      res.render("inbox", { emails: data });
-    }).catch(function(err) {
-      res.status(401).json(err);
-    });
+      where: { recipientID: req.user.id }
+    })
+      .then(data => {
+        res.render("inbox", { emails: data });
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
   });
 
   app.get("/outbox", isAuthenticated, function(req, res) {
     db.Email.findAll({
-      where : {UserId: req.user.id }
-    }).then(data => {
-      res.render("outbox", { emails: data });
-    }).catch(function(err) {
-      res.status(401).json(err);
-    });
+      where: { UserId: req.user.id }
+    })
+      .then(data => {
+        res.render("outbox", { emails: data });
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
   });
 };
