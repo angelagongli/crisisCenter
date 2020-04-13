749337586db18a909f364540772d8f1205d7379e
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-04-12 20:29:50 -0500

    Trying to fix stuff.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,10 +3,8 @@ var path = require("path");
 var db = require("../models");
 var axios = require("axios");
 var Twitter = require("twitter");
-
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
-
 module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
@@ -15,7 +13,6 @@ module.exports = function(app) {
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
-
   app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
@@ -23,24 +20,16 @@ module.exports = function(app) {
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
-
   app.get("/members", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
-
   app.get("/staybusy", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/staybusy.html"));
   });
-
   app.get("/forum", isAuthenticated, function(req, res) {
     // If the user is logged in, send them to the forum page
     res.sendFile(path.join(__dirname, "../public/forum.html"));
   });
-  app.get("/post", isAuthenticated, function(req, res) {
-    // If the user is logged in, send them to the forum page
-    res.sendFile(path.join(__dirname, "../public/post.html"));
-  });
-
   app.get("/bookmarks", isAuthenticated, function(req, res) {
     db.Bookmark.findAll({
       where: { UserId: req.user.id }
@@ -52,7 +41,6 @@ module.exports = function(app) {
         res.status(401).json(err);
       });
   });
-
   app.get("/inbox", isAuthenticated, function(req, res) {
     db.Email.findAll({
       where: { recipientID: req.user.id }
@@ -64,7 +52,6 @@ module.exports = function(app) {
         res.status(401).json(err);
       });
   });
-
   app.get("/outbox", isAuthenticated, function(req, res) {
     db.Email.findAll({
       where: { UserId: req.user.id }
@@ -75,5 +62,30 @@ module.exports = function(app) {
       .catch(function(err) {
         res.status(401).json(err);
       });
+  });
+  app.get("/staybusy", isAuthenticated, function(req, res) {
+    db.Idea.findAll({
+      where: { UserId: req.user.id }
+    })
+      .then(data => {
+        res.render("newIdea", { ideas: data });
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
+  });
+  app.post("/staybusy", function(req, res) {
+    db.Idea.create({
+      title: req.body.title,
+      body: req.body.body,
+      user: req.body.user
+    }).then(dbPost => res.json({ dbPost }));
+  });
+  app.get("/forum/:id", function(req, res) {
+    db.Post.findOne({
+      where: { id: req.params.id }
+    }).then(post => {
+      app.render("post", { title: post.title, body: post.body });
+    });
   });
 };
